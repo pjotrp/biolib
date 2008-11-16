13ec197f1e4d3622f4e48c6a98cc92c4000d8b29
pjotrp <pjotr.public01@thebird.nl>
2008-11-16 10:41:00 +0100

    affy C example: load CEL file from command line

--- doc/examples/c/affy.c
+++ doc/examples/c/affy.c
@@ -1,12 +1,20 @@
 /* BioLib Affymetrix C example */
 
+#include <stdio.h>
+#include <stdlib.h>
 #include <biolib_affyio.h>
 #include <biolib_microarray.h>
 
-int main()
+int main(int argc, char * argv[])
 {
    CELOBJECT *cel;
-   cel = open_celfile("/tmp/test_binary.cel.gz");
-   // cel = open_celfile("test46.CEL.gz.stub");
+   char *fn;
+
+   while(--argc) {
+     fn = *++argv;   
+     printf("Loading CEL file: %s\n", fn);
+     cel = open_celfile(fn);
+   }
+   exit(EXIT_SUCCESS);
 }
 
