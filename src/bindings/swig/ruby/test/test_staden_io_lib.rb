1856dba67a3de1f2b83c39bf6c972fd69ee25d08
pjotrp <pjotr.public01@thebird.nl>
2009-01-31 11:43:15 +0100

    staden-io_lib reads ztr test file into Ruby - display output of trace record.

--- src/bindings/swig/ruby/test/test_staden_io_lib.rb
+++ src/bindings/swig/ruby/test/test_staden_io_lib.rb
@@ -7,4 +7,8 @@ datadir = '../../../../test/data/trace'
 procsrffn = datadir+'/abi3700-ztr/1dJ671C13-2a01.q1c.ztr'
 raise 'Error' if !File.exist?(procsrffn)
 result = Biolib::Staden_io_lib.read_reading(procsrffn,Biolib::Staden_io_lib::TT_ANY)
+p result.format
+p result.NBases
+p result.base
+
 exit 0
