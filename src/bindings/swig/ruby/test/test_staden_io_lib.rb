9d659a54278b5aa476bc4b7b3c83bf09de20b4a8
pjotrp <pjotr.public01@thebird.nl>
2009-01-31 11:38:25 +0100

    staden-io_lib reads ztr test file into Ruby

--- src/bindings/swig/ruby/test/test_staden_io_lib.rb
+++ src/bindings/swig/ruby/test/test_staden_io_lib.rb
@@ -1,6 +1,10 @@
 
 require 'staden_io_lib'
 
-# result = Biolib::Example.my_mod(7,3)
-# print "Biolib::Example.my_mod(7,3) renders #{result}\n"
+# $top_builddir/progs/srf2fasta $outdir/proc.srf > $outdir/slx.fasta 
+
+datadir = '../../../../test/data/trace'
+procsrffn = datadir+'/abi3700-ztr/1dJ671C13-2a01.q1c.ztr'
+raise 'Error' if !File.exist?(procsrffn)
+result = Biolib::Staden_io_lib.read_reading(procsrffn,Biolib::Staden_io_lib::TT_ANY)
 exit 0
