b678d7bfbc833b7e0a0be850a85fd2bf92806b25
pjotrp <pjotr.public01@thebird.nl>
2009-02-01 10:35:09 +0100

    Introduce subset builds - e.g.
      cmake -DSTADEN_LIB:BOOLEAN=TRUE -DBUILD_PERL:BOOLEAN=TRUE .
    
    builds the Staden-io-lib library for Perl only.

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
+#
+# You can pass parameters to CMake. E.g.
+#
+#   sh install.sh -DSTADEN_LIB:BOOLEAN=TRUE -DBUILD_PERL:BOOLEAN=TRUE .
 
 if [ ! -d cmake_modules ]; then
   echo "Only run from BioLib top-level directory"
@@ -6,7 +10,7 @@ if [ ! -d cmake_modules ]; then
 fi
 
 sh scripts/cleanup.sh
-cmake .
+cmake $* .
 make
 make test
 echo "Give root password to install libraries - or press Ctrl-C"
