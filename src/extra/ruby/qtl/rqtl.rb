0ce3375aa84b925144b0c8eaf40cf6edc71e2602
pjotrp <pjotr.public01@thebird.nl>
2009-03-07 11:08:51 +0100

    Preparing to map rqtl

--- src/extra/ruby/qtl/rqtl.rb
+++ src/extra/ruby/qtl/rqtl.rb
@@ -1,5 +1,6 @@
 # RQTL convenience wrapper for Biolib R/QTL mappings
 
+require 'biolib/rqtl'
 
 class RQTL
 
@@ -7,9 +8,30 @@ class RQTL
     @qtl = qtl
   end
 
-  # Call R/QTL scanone method with options.
+  # Calls directly into R/QTL the scanone method with options and returns
+  # a list of markers with chromosome positions and lod scores.
 
   def scanone options
+
+    # call the C function
+    if (:method == "mr" or :method=="mr-imp" or :method=="mr-argmax")
+=begin
+       z <- .C("R_scanone_mr",
+              as.integer(n.ind),         # number of individuals
+              as.integer(n.pos),         # number of markers
+              as.integer(n.gen),         # number of possible genotypes
+              as.integer(newgeno),       # genotype data
+              as.double(ac),       # additive covariates
+              as.integer(n.ac),
+              as.double(ic),       # interactive covariates
+              as.integer(n.ic),
+              as.double(pheno),          # phenotype data
+              as.double(weights),        # weights
+              result=as.double(rep(0,n.pos)),
+              PACKAGE="qtl")
+=end
+      res = Biolib::rqtl.R_scanone_mr()
+    end
   end
 
 end
