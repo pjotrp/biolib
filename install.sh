a3de2f3619d05b613dc15280773c341bf3a33267
pjotrp <pjotr.public01@thebird.nl>
2009-05-05 14:00:32 +0200

    Release 0.0.4

--- install.sh
+++ install.sh
@@ -4,20 +4,26 @@
 #
 # to build for Perl:
 #
-#   ./install.sh -DBUILD_PERL:BOOLEAN=TRUE 
+#   ./install.sh
 #
 # to build one library mapping for Perl:
 #
-#   ./install.sh -DSTADEN_LIB:BOOLEAN=TRUE -DBUILD_PERL:BOOLEAN=TRUE 
+#   ./install.sh --with-staden
 #
+# to build for Ruby:
+#
+#   ./install.sh --with-ruby
+#
+# to build for Python
+#
+#   ./install.sh --with-python
 
-if [ ! -d cmake_modules ]; then
+if [ ! -e PROJECTNAME ]; then
   echo "Only run from BioLib top-level directory"
   exit 1
 fi
 
-sh scripts/cleanup.sh
-cmake $* .
+./configure $*
 make
 make test
 echo "-- No errors? Run 'make install' to complete the installation"
