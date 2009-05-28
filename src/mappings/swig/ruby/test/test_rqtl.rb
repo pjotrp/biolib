758185a7941afe4560006240aafacb8f34e77df2
pjotrp <pjotr.public01@thebird.nl>
2009-05-28 12:11:03 +0200

    Print result in test (empty now)

--- src/mappings/swig/ruby/test/test_rqtl.rb
+++ src/mappings/swig/ruby/test/test_rqtl.rb
@@ -3,8 +3,8 @@ require 'biolib/rqtl'
 
 if $0 == __FILE__
 
-  Biolib::Rqtl.scanone_mr(0,0,0,nil,nil,0,nil,0,[],[],[])
-  print "R/qtl parameters not yet working"
+  res = Biolib::Rqtl.scanone_mr(0,0,0,nil,nil,0,nil,0,[],[],[])
+  print "result = #{res}"
   print "Success!"
   exit 0
 end
