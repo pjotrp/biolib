a53d046ad29b987e06bc7ba2005e463e07386e23
pjotrp <pjotr.public01@thebird.nl>
2009-06-15 14:15:03 +0200

    Rlib functions

--- src/mappings/swig/biolib_R.i
+++ src/mappings/swig/biolib_R.i
@@ -1,3 +1,5 @@
+#define HAVE_LOG1P 1
+
 %{
   #include <biolib_R_map.h>
   #include <R_ext/Memory.h>
--- src/mappings/swig/ruby/test/test_biolib_R.rb
+++ src/mappings/swig/ruby/test/test_biolib_R.rb
@@ -9,7 +9,12 @@ map = Biolib::Biolib_R.R_alloc(2,2)
 
 # test native math functions
 
-random = Biolib::Biolib_R.Rf_log(30)
-p random
+print "factorial(10): "
+p Biolib::Biolib_R.Rf_gammafn(10+1)
+print "Ggamma:\n"
+p Biolib::Biolib_R.Rf_rgamma(0.5,2)
+p Biolib::Biolib_R.Rf_gammafn(0.5)
+print "log(2): "
+p Biolib::Biolib_R.Rlog1p(1)
 
 exit 0
