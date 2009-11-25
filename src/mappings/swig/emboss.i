01ffdbcfe38aee70b6e770b86129f771f078efbe
pjotrp <pjotr.public01@thebird.nl>
2009-11-25 17:09:37 +0100

    Calling translation NT -> AA works for the EMBOSS mapping

--- src/mappings/swig/emboss.i
+++ src/mappings/swig/emboss.i
@@ -10,7 +10,9 @@
 %include <ajax.h>
 %include <ajarch.h>
 %include <ajseq.h>
+# %include <ajseqdata.h>
 # %include <ajtranslate.h>
 
-AjPTrn        ajTrnNewI (ajint trnFileNameInt);
+AjPTrn ajTrnNewI (ajint trnFileNameInt);
+AjPSeq ajTrnSeqOrig (const AjPTrn trnObj, const AjPSeq seq, ajint frame);
 
--- src/mappings/swig/ruby/test/test_emboss.rb
+++ src/mappings/swig/ruby/test/test_emboss.rb
@@ -1,8 +1,11 @@
 
 require 'biolib/emboss'
 
+ENV['DATA'] = "../../../../test/data/emboss/"
 trnTable = Biolib::Emboss.ajTrnNewI(1);
-
-# raise 'Test failed' if result != 1
+ajpseq   = Biolib::Emboss.ajSeqNewNameC("AGCT","Test sequence")
+ajpseqt  = Biolib::Emboss.ajTrnSeqOrig(trnTable,ajpseq,1)
+aa       = Biolib::Emboss.ajSeqGetSeqCopyC(ajpseqt)
+raise 'Test failed '+aa if aa != 'SX'
 
 exit 0
