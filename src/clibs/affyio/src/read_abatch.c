39c4fa8c138b11d9cd17d48164f7ff447dd9d324
pjotrp <pjotr.public01@thebird.nl>
2010-01-27 11:55:56 +0100

    Affyio: Moved CEL definition into header file

--- src/clibs/affyio/src/read_abatch.c
+++ src/clibs/affyio/src/read_abatch.c
@@ -206,39 +206,6 @@ struct thread_data{
 #define BUF_SIZE 1024
 
 
-/******************************************************************
- **
- ** A "C" level object designed to hold information for a
- ** single CEL file
- **
- ** These should be created using the function
- **
- ** read_cel_file()
- **
- **
- **
- *****************************************************************/
-
-typedef struct{
-  detailed_header_info header;
-  
-  /** these are for storing the intensities, the sds and the number of pixels **/
-  double *intensities;
-  double *stddev;
-  double *npixels;
-
-  /** these are for storing information in the masks and outliers section **/
-  
-  int nmasks;
-  int noutliers;
-
-  short *masks_x, *masks_y;
-  short *outliers_x, *outliers_y;
-
-} CEL;
-
-
-
 
 
 
--- src/clibs/affyio/src/read_abatch.h
+++ src/clibs/affyio/src/read_abatch.h
@@ -25,5 +25,39 @@ typedef struct{
   char *ScanDate;
 } detailed_header_info;
 
+/******************************************************************
+ **
+ ** A "C" level object designed to hold information for a
+ ** single CEL file
+ **
+ ** These should be created using the function
+ **
+ ** read_cel_file()
+ **
+ **
+ **
+ *****************************************************************/
+
+typedef struct{
+  detailed_header_info header;
+  
+  /** these are for storing the intensities, the sds and the number of pixels **/
+  double *intensities;
+  double *stddev;
+  double *npixels;
+
+  /** these are for storing information in the masks and outliers section **/
+  
+  int nmasks;
+  int noutliers;
+
+  short *masks_x, *masks_y;
+  short *outliers_x, *outliers_y;
+
+} CEL;
+
+extern CEL *read_cel_file(const char *filename, int read_intensities_only);
+
+
 
 #endif
