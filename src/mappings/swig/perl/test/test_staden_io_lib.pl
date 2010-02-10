af6a2cced785ccc59ed526e001bd1d7918337b69
pjotrp <pjotr.public01@thebird.nl>
2010-02-10 11:37:14 +0900

    Staden_io_lib: Fixed the multi read_read, which gets a file pointer passed in.                It lacked 2 header files and a Perl test.

--- src/mappings/swig/perl/test/test_staden_io_lib.pl
+++ src/mappings/swig/perl/test/test_staden_io_lib.pl
@@ -2,11 +2,12 @@ use biolib::staden_io_lib;
 
 $datadir = '../../../../test/data/trace';
 
-# Read test data
+# Read test data - reading one record from file
 $procsrffn = "$datadir/abi3700-ztr/1dJ671C13-2a01.q1c.ztr";
 print "Parsing trace file ",$procsrffn;
 
 print "\nTT_ZTR=",$staden_io_lib::TT_ZTR;
+
 $result = staden_io_lib::read_reading($procsrffn,$staden_io_lib::TT_ANY);
 
 print("\nformat=",staden_io_libc::Read_format_get($result));
@@ -15,9 +16,39 @@ print("\nbase=",staden_io_libc::Read_base_get($result));
 
 croak('Read test failed') if $result->{NBases} != 766;
 
-# Now write test SCF file
+# Now write record to test SCF file
 $wresult = staden_io_lib::write_reading('test.scf', $result, 0);
 croak('Write test failed') if $wresult != 0 or ! -e 'test.scf';
 unlink('test.scf');
+
+# Let's try a multi-read using a stateful file pointer, like the example in
+# staden's ./progs/extract_fastq.c. It repeats 
+#
+#   Read *r = mfread_reading(mFILE *infp, char *in_file, int format)
+#
+# over a trace file, opened as 
+#
+#   mFile *infp = open_exp_mfile(char *fn, char *path);
+#
+# or
+#
+#   mFile *infp = open_trace_mfile(char *fn, char *path);
+#
+# In fact, read_read also uses mfread_read, but only once (see Read.c).
+#
+# In Perl:
+
+$infp = staden_io_lib::open_exp_mfile($procsrffn,"");
+$result = staden_io_lib::mfread_reading($infp,$procsrffn,$staden_io_lib::TT_ANY);
+print("\nNBases=",$result->{NBases});
+print("\nbase=",staden_io_libc::Read_base_get($result));
+
+# and another time
+$result = staden_io_lib::mfread_reading($infp,$procsrffn,$staden_io_lib::TT_ANY);
+print("\nNBases=",$result->{NBases});
+print("\nbase=",staden_io_libc::Read_base_get($result));
+
+# close the file
+staden_io_lib::mfclose($infp);
 
 exit 0;
